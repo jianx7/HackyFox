5c31517a76b11411394d787b5797eaccee95a66d
Rubi468 <24393220@utcancun.edu.mx>
2025-07-14 10:53:14 -0500

    Dinamicas 2, 4 y 5 corregidas

---
+++ HackyFox/Dinamica2.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace HackyFox
+{
+    partial class Dinamica2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pMenuD2 = new Panel();
+            pNombreD2 = new Panel();
+            lbTituloD2 = new Label();
+            lbInstruccionesD2 = new Label();
+            lbNombreD2 = new Label();
+            btn_D2R1 = new Button();
+            btn_D2R2 = new Button();
+            btn_D2R3 = new Button();
+            btn_D2R4 = new Button();
+            pictureBox1 = new PictureBox();
+            pNombreD2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pMenuD2
+            // 
+            pMenuD2.Dock = DockStyle.Left;
+            pMenuD2.Location = new Point(0, 0);
+            pMenuD2.Name = "pMenuD2";
+            pMenuD2.Size = new Size(100, 726);
+            pMenuD2.TabIndex = 0;
+            // 
+            // pNombreD2
+            // 
+            pNombreD2.BackColor = Color.FromArgb(155, 209, 219);
+            pNombreD2.Controls.Add(lbNombreD2);
+            pNombreD2.Dock = DockStyle.Top;
+            pNombreD2.Location = new Point(100, 0);
+            pNombreD2.Name = "pNombreD2";
+            pNombreD2.Size = new Size(1008, 70);
+            pNombreD2.TabIndex = 1;
+            // 
+            // lbTituloD2
+            // 
+            lbTituloD2.AutoSize = true;
+            lbTituloD2.Font = new Font("Clear Sans", 36F, FontStyle.Bold);
+            lbTituloD2.ForeColor = Color.FromArgb(242, 139, 12);
+            lbTituloD2.Location = new Point(121, 85);
+            lbTituloD2.Name = "lbTituloD2";
+            lbTituloD2.Size = new Size(803, 84);
+            lbTituloD2.TabIndex = 2;
+            lbTituloD2.Text = "¿Compartir o no compartir?";
+            // 
+            // lbInstruccionesD2
+            // 
+            lbInstruccionesD2.AutoSize = true;
+            lbInstruccionesD2.Font = new Font("Comfortaa", 14F);
+            lbInstruccionesD2.ForeColor = Color.FromArgb(0, 74, 173);
+            lbInstruccionesD2.Location = new Point(177, 169);
+            lbInstruccionesD2.Name = "lbInstruccionesD2";
+            lbInstruccionesD2.Size = new Size(679, 38);
+            lbInstruccionesD2.TabIndex = 3;
+            lbInstruccionesD2.Text = "Haz clic en la opción que SÍ compartirías en internet.";
+            // 
+            // lbNombreD2
+            // 
+            lbNombreD2.AutoSize = true;
+            lbNombreD2.Font = new Font("Clear Sans", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lbNombreD2.ForeColor = Color.White;
+            lbNombreD2.Location = new Point(6, 18);
+            lbNombreD2.Name = "lbNombreD2";
+            lbNombreD2.Size = new Size(314, 39);
+            lbNombreD2.TabIndex = 0;
+            lbNombreD2.Text = "Lección 2. Mi Yo Digital";
+            // 
+            // btn_D2R1
+            // 
+            btn_D2R1.Image = Properties.Resources.btn_Dinamica2;
+            btn_D2R1.Location = new Point(199, 257);
+            btn_D2R1.Name = "btn_D2R1";
+            btn_D2R1.Size = new Size(264, 406);
+            btn_D2R1.TabIndex = 4;
+            btn_D2R1.UseVisualStyleBackColor = true;
+            btn_D2R1.Click += btn_D2R1_Click;
+            // 
+            // btn_D2R2
+            // 
+            btn_D2R2.Image = Properties.Resources.btn1_Dinamica2;
+            btn_D2R2.Location = new Point(496, 257);
+            btn_D2R2.Name = "btn_D2R2";
+            btn_D2R2.Size = new Size(264, 406);
+            btn_D2R2.TabIndex = 5;
+            btn_D2R2.UseVisualStyleBackColor = true;
+            // 
+            // btn_D2R3
+            // 
+            btn_D2R3.Image = Properties.Resources.btn2_Dinamica2;
+            btn_D2R3.Location = new Point(798, 240);
+            btn_D2R3.Name = "btn_D2R3";
+            btn_D2R3.Size = new Size(0, 0);
+            btn_D2R3.TabIndex = 6;
+            btn_D2R3.UseVisualStyleBackColor = true;
+            // 
+            // btn_D2R4
+            // 
+            btn_D2R4.Image = Properties.Resources.btn2_Dinamica2;
+            btn_D2R4.Location = new Point(798, 257);
+            btn_D2R4.Name = "btn_D2R4";
+            btn_D2R4.Size = new Size(264, 406);
+            btn_D2R4.TabIndex = 7;
+            btn_D2R4.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.edit_cuidado;
+            pictureBox1.Location = new Point(843, 76);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(312, 185);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 8;
+            pictureBox1.TabStop = false;
+            // 
+            // Dinamica2
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1108, 726);
+            Controls.Add(btn_D2R4);
+            Controls.Add(btn_D2R3);
+            Controls.Add(btn_D2R2);
+            Controls.Add(btn_D2R1);
+            Controls.Add(lbInstruccionesD2);
+            Controls.Add(lbTituloD2);
+            Controls.Add(pNombreD2);
+            Controls.Add(pMenuD2);
+            Controls.Add(pictureBox1);
+            MinimumSize = new Size(950, 600);
+            Name = "Dinamica2";
+            Text = "Dinamica2";
+            pNombreD2.ResumeLayout(false);
+            pNombreD2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel pMenuD2;
+        private Panel pNombreD2;
+        private Label lbNombreD2;
+        private Label lbTituloD2;
+        private Label lbInstruccionesD2;
+        private Button btn_D2R1;
+        private Button btn_D2R2;
+        private Button btn_D2R3;
+        private Button btn_D2R4;
+        private PictureBox pictureBox1;
+    }
+}
---
+++ HackyFox/Dinamica4.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace HackyFox
+{
+    partial class Dinamica4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pMenuD4 = new Panel();
+            pNombreD4 = new Panel();
+            lbNombreD4 = new Label();
+            lbTituloD4 = new Label();
+            lbInstruccionesD4 = new Label();
+            btn_D4R1 = new Button();
+            btn_D4R2 = new Button();
+            btn_D4R3 = new Button();
+            btn_D4R4 = new Button();
+            btn_D4R5 = new Button();
+            btn_D4R6 = new Button();
+            pBMascotaD4 = new PictureBox();
+            pNombreD4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pBMascotaD4).BeginInit();
+            SuspendLayout();
+            // 
+            // pMenuD4
+            // 
+            pMenuD4.Dock = DockStyle.Left;
+            pMenuD4.Location = new Point(0, 0);
+            pMenuD4.Name = "pMenuD4";
+            pMenuD4.Size = new Size(100, 726);
+            pMenuD4.TabIndex = 0;
+            // 
+            // pNombreD4
+            // 
+            pNombreD4.BackColor = Color.FromArgb(155, 209, 219);
+            pNombreD4.Controls.Add(lbNombreD4);
+            pNombreD4.Dock = DockStyle.Top;
+            pNombreD4.Location = new Point(100, 0);
+            pNombreD4.Name = "pNombreD4";
+            pNombreD4.Size = new Size(1008, 70);
+            pNombreD4.TabIndex = 1;
+            // 
+            // lbNombreD4
+            // 
+            lbNombreD4.AutoSize = true;
+            lbNombreD4.Font = new Font("Clear Sans", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lbNombreD4.ForeColor = Color.White;
+            lbNombreD4.Location = new Point(3, 19);
+            lbNombreD4.Name = "lbNombreD4";
+            lbNombreD4.Size = new Size(386, 39);
+            lbNombreD4.TabIndex = 0;
+            lbNombreD4.Text = "Lección 4. Protege tu Correo";
+            // 
+            // lbTituloD4
+            // 
+            lbTituloD4.AutoSize = true;
+            lbTituloD4.Font = new Font("Clear Sans", 36F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lbTituloD4.ForeColor = Color.FromArgb(242, 139, 12);
+            lbTituloD4.Location = new Point(284, 92);
+            lbTituloD4.Name = "lbTituloD4";
+            lbTituloD4.Size = new Size(618, 84);
+            lbTituloD4.TabIndex = 2;
+            lbTituloD4.Text = "Detectives del buzón ";
+            // 
+            // lbInstruccionesD4
+            // 
+            lbInstruccionesD4.AutoSize = true;
+            lbInstruccionesD4.Font = new Font("Comfortaa", 14F);
+            lbInstruccionesD4.ForeColor = Color.FromArgb(0, 74, 173);
+            lbInstruccionesD4.Location = new Point(373, 185);
+            lbInstruccionesD4.Name = "lbInstruccionesD4";
+            lbInstruccionesD4.Size = new Size(379, 38);
+            lbInstruccionesD4.TabIndex = 3;
+            lbInstruccionesD4.Text = "Reconoce el correo confiable";
+            // 
+            // btn_D4R1
+            // 
+            btn_D4R1.Image = Properties.Resources.D2R3;
+            btn_D4R1.Location = new Point(382, 295);
+            btn_D4R1.Name = "btn_D4R1";
+            btn_D4R1.Size = new Size(141, 104);
+            btn_D4R1.TabIndex = 4;
+            btn_D4R1.UseVisualStyleBackColor = true;
+            // 
+            // btn_D4R2
+            // 
+            btn_D4R2.Image = Properties.Resources.D2R2;
+            btn_D4R2.Location = new Point(611, 295);
+            btn_D4R2.Name = "btn_D4R2";
+            btn_D4R2.Size = new Size(141, 104);
+            btn_D4R2.TabIndex = 5;
+            btn_D4R2.UseVisualStyleBackColor = true;
+            // 
+            // btn_D4R3
+            // 
+            btn_D4R3.Image = Properties.Resources.D2R4;
+            btn_D4R3.Location = new Point(849, 295);
+            btn_D4R3.Name = "btn_D4R3";
+            btn_D4R3.Size = new Size(141, 104);
+            btn_D4R3.TabIndex = 6;
+            btn_D4R3.UseVisualStyleBackColor = true;
+            // 
+            // btn_D4R4
+            // 
+            btn_D4R4.Image = Properties.Resources.D2R5;
+            btn_D4R4.Location = new Point(382, 521);
+            btn_D4R4.Name = "btn_D4R4";
+            btn_D4R4.Size = new Size(141, 104);
+            btn_D4R4.TabIndex = 7;
+            btn_D4R4.UseVisualStyleBackColor = true;
+            // 
+            // btn_D4R5
+            // 
+            btn_D4R5.Image = Properties.Resources.D2R6;
+            btn_D4R5.Location = new Point(611, 521);
+            btn_D4R5.Name = "btn_D4R5";
+            btn_D4R5.Size = new Size(141, 104);
+            btn_D4R5.TabIndex = 8;
+            btn_D4R5.UseVisualStyleBackColor = true;
+            // 
+            // btn_D4R6
+            // 
+            btn_D4R6.Image = Properties.Resources.D2R7;
+            btn_D4R6.Location = new Point(849, 521);
+            btn_D4R6.Name = "btn_D4R6";
+            btn_D4R6.Size = new Size(141, 104);
+            btn_D4R6.TabIndex = 9;
+            btn_D4R6.UseVisualStyleBackColor = true;
+            // 
+            // pBMascotaD4
+            // 
+            pBMascotaD4.Image = Properties.Resources.D4Mascota;
+            pBMascotaD4.Location = new Point(-18, 318);
+            pBMascotaD4.Name = "pBMascotaD4";
+            pBMascotaD4.Size = new Size(378, 480);
+            pBMascotaD4.SizeMode = PictureBoxSizeMode.Zoom;
+            pBMascotaD4.TabIndex = 10;
+            pBMascotaD4.TabStop = false;
+            // 
+            // Dinamica4
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1108, 726);
+            Controls.Add(btn_D4R6);
+            Controls.Add(btn_D4R5);
+            Controls.Add(btn_D4R4);
+            Controls.Add(btn_D4R3);
+            Controls.Add(btn_D4R2);
+            Controls.Add(btn_D4R1);
+            Controls.Add(lbInstruccionesD4);
+            Controls.Add(lbTituloD4);
+            Controls.Add(pNombreD4);
+            Controls.Add(pMenuD4);
+            Controls.Add(pBMascotaD4);
+            MinimumSize = new Size(950, 600);
+            Name = "Dinamica4";
+            Text = "Dinamica4";
+            pNombreD4.ResumeLayout(false);
+            pNombreD4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pBMascotaD4).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel pMenuD4;
+        private Panel pNombreD4;
+        private Label lbNombreD4;
+        private Label lbTituloD4;
+        private Label lbInstruccionesD4;
+        private Button btn_D4R1;
+        private Button btn_D4R2;
+        private Button btn_D4R3;
+        private Button btn_D4R4;
+        private Button btn_D4R5;
+        private Button btn_D4R6;
+        private PictureBox pBMascotaD4;
+    }
+}
